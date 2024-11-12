5b95f70bb5a79b587ab0f7ef658e441d922fc7a1
shishir.yadav@chargebee.com <shishir.yadav@chargebee.com>
2024-11-12 08:38:17 +0530

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2MzkyMQ/spectral.js?branch_slug=main&token=565e6dd0-5999-4eb4-9ec8-8a47f80f8396";
+export default { extends: ruleset };
